c7a3d28f90217fd82b19b0b49175c3903d321b6e
Galaceon <antoniogarcialtr@gmail.com>
2025-03-19 23:58:25 +0100

    script updated

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
 const heading = document.querySelector(".header__texto h2")
 heading.textContent = "Ejemplo para probar";
-console.log(heading)
+
+
+/** QueryAll **/
+const enlaces = document.querySelectorAll('.navegacion a')
+enlaces[0].textContent = 'para probar'
+enlaces[0].classList.add('nueva-clase')
+enlaces[0].classList.remove('navegacion__enlace')
