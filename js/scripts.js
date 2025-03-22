9314eab1fa46879c6b3c08d29c642e1e8dcc6234
Galaceon <antoniogarcialtr@gmail.com>
2025-03-22 18:12:24 +0100

    event and HTMLcreation

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,28 @@ const enlaces = document.querySelectorAll('.navegacion a')
 enlaces[0].textContent = 'para probar'
 enlaces[0].classList.add('nueva-clase')
 enlaces[0].classList.remove('navegacion__enlace')
+
+/** Crear Elementos HTML **/
+const nuevoEnlace = document.createElement('A');
+
+nuevoEnlace.href = 'nuevo-enlace.html';
+
+nuevoEnlace.textContent = 'Tienda Virtual';
+
+nuevoEnlace.classList.add('navegacion__enlace');
+
+const nav = document.querySelector('.navegacion');
+nav.appendChild(nuevoEnlace)
+
+
+/** Eventos en JS **/
+window.onscroll = function() {
+    console.log('scrolling')
+}
+
+const btnEnviar = document.querySelector('.boton');
+
+btnEnviar.addEventListener('click', function(e) {
+    e.preventDefault();
+    console.log('hola')
+})
